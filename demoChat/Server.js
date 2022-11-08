9c95211281f2ef91d904c5ae719de8c886a28345
nguyen_trung_dung <98233891+mrdzung92@users.noreply.github.com>
2022-11-08 23:29:57 +0800

    1

--- demoChat/Server.js
+++ demoChat/Server.js
@@ -7,17 +7,29 @@ var server = require("http").Server(app);
 var io = require("socket.io")(server);
 server.listen(3000);
 var arrUser =[];
+// lang nghe su kien co nguoi ket noi
 io.on('connection',function(socket){
     console.log('Co nguoi ket noi: '+socket.id)
+    // lang nghe va xu li su kien co nguoi dang ky
   socket.on('client-send-user-name',function(data){
-    if(data.indexOf(data)!=0){
+    if(arrUser.indexOf(data)>=0){
         socket.emit('server-send-fail');
     }else{
         arrUser.push(data);
+        socket.Username = data;
         socket.emit('server-send-success',data);
-        console.log(socket)
+        io.sockets.emit('server-send-list-online',arrUser);
+      
     }
-    
+    //lang nghe va xu ly co nguoi thoat
+    socket.on('logout',function(){
+      arrUser.splice(arrUser.indexOf(socket.Username),1);   
+      socket.broadcast.emit('server-send-list-online',arrUser);
+    });
+    //lang nghe client gui message
+    socket.on('client-send-message',function(data){
+      io.sockets.emit('server-send-message',{Username:socket.Username,content:data});
+    });
   })  
 })
 
--- demoChat/public/xuly.js
+++ demoChat/public/xuly.js
@@ -3,15 +3,45 @@ socket.on('server-send-fail',function(){
     alert('co nguoi dang ky roi')
 })
 socket.on('server-send-success',function(data){
-   $('current-user').html(data);
-   $('#login-form').hide();
-   $('#chat-form').show();
-   console.log(socket)
+   $('#current-user').html(data);
+   $('#login-form').hide(2000);
+   $('#chat-form').show(1000);
+   
 })
+
+
+
+socket.on('server-send-list-online',function(data){
+   $('#box-content').html('');
+   data.forEach(element => {
+      $('#box-content').append('<div class="user-online">'+element+'</div>')
+   });
+})
+
+socket.on('server-send-message',function(data){
+  $('#list-message').append('<div class="ms-content">'+data.Username+' : '+ data.content+'</div>');
+})
+
+
 $(document).ready(function(){
    $('#login-form').show();
    $('#chat-form').hide();
    $('#btn-register').click(function(){
     socket.emit('client-send-user-name',$('#user-name').val())
    })
+   $('#btn-logout').click(function(){
+      socket.emit('logout')
+      $('#login-form').show(1000);
+      $('#chat-form').hide(2000);
+     })
+
+     $('#btn-send-message').click(function(){
+     
+      var text = '';   
+      if($('#txt-message').val()!=''){
+         text =$('#txt-message').val();
+         socket.emit('client-send-message',text);
+         $('#txt-message').val('');
+      }
+     })
 })
